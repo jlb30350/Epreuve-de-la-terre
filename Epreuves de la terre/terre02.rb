3c899c23cedf0a425342354a5762732b29af5426
jlb30350 <112830521+jlb30350@users.noreply.github.com>
2024-03-21 23:00:20 +0100

    Update terre02.rb

--- Epreuves de la terre/terre02.rb
+++ Epreuves de la terre/terre02.rb
@@ -1,12 +1,11 @@
 #5
 #Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
 
-
-def voici_mes_arguments(*array_arguments)
-  array_arguments.each do |motendessouslundelautre|
-    puts motendessouslundelautre
-  end
+ARGV.each do |argument|
+  puts argument
 end
 
-voici_mes_arguments("je", "suis", "bonneville", "jean", "luc", "j", " ", "ai", "la", "win")
+arguments("je", "suis", "bonneville", "jean", "luc", "j", " ", "ai", "la", "win")
+
+
 
